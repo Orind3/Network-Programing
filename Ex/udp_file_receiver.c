1865a32e40d3490f764a78baf28d84aa6f8936b3
hieu <backinh74@gmail.com>
2023-04-17 16:08:41 +0700

    Ex3 done

---
+++ Ex/udp_file_receiver.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<time.h>
+
+typedef struct sockaddr_in sockaddr_in;
+
+
+int main(int argc,char * argv[]){
+    sockaddr_in server;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(atoi(argv[1]));
+    //Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(sockfd==-1){
+        printf("Error: %d - %s\n",errno, strerror(errno));
+        printf("1\n");
+        exit(1);
+    }
+    socklen_t server_len = sizeof(server);
+    if(bind(sockfd,(struct sockaddr *)&server, server_len)){
+        printf("Error: %d - %s\n",errno, strerror(errno));
+        printf("2\n");
+        exit(1);
+    }
+
+    char buf[1000];
+
+    while(1){
+        int data_rec = recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&server,&server_len);
+        printf("%d - %s\n",data_rec,buf);
+        if(data_rec==-1){
+            printf("Error: %d - %s\n",errno, strerror(errno));
+            exit(1);
+        }
+        buf[data_rec] = 0;
+        break;
+    }
+    char file[1000];
+    char cont[1000];
+    char * space = strchr(buf,' ');
+    strcpy(cont,space+1);
+    *space = 0;
+    strcpy(file,buf);
+    FILE * ptr = fopen(file,"w");
+    fprintf(ptr,"%s",cont);
+    fclose(ptr);
+    close(sockfd);
+}
---
+++ Ex/udp_file_sender.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<time.h>
+
+
+typedef struct sockaddr_in sockaddr_in;
+
+
+
+int main(int argc,char * argv[]){
+    if(argc!=4){
+        printf("Please use format: <IP address> <Port> <File name>\n");
+        exit(1);
+    }
+    sockaddr_in client;
+    client.sin_addr.s_addr = inet_addr(argv[1]);
+    client.sin_family = AF_INET;
+    client.sin_port = htons(atoi(argv[2]));
+    //Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if(sockfd==-1){
+        printf("Error: %d - %s\n",errno, strerror(errno));
+        exit(1);
+    }
+
+    while(1){
+        char cont[256];
+        printf("Enter content for file: \n");
+        scanf("%[^\n]%*c",cont);
+        char buf[1000];
+        sprintf(buf,"%s %s",argv[3],cont);
+        int data_send = sendto(sockfd,buf,strlen(buf),0,(struct sockaddr*)&client,sizeof(client));
+        if(data_send==-1){
+            printf("Error: %d - %s\n",errno,strerror(errno));
+            break;
+        }
+        break;
+    }
+    close(sockfd);
+}
