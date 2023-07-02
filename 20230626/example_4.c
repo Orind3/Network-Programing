af77029958925676de90ebe424fc24271dbc3658
Orind3 <104160012+Orind3@users.noreply.github.com>
2023-07-02 23:11:40 +0700

    Add files via upload
    add Ex2606

---
+++ 20230626/example_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] > ans) {
+                ans = arr[i] + arr[j];
+            }
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
